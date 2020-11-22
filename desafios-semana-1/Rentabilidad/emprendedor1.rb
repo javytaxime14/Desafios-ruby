665d9db7636a3a4cb6ec3975ea7eac295bc6f1e2
Javiera Hidalgo <javiera_56@hotmail.com>
2020-11-22 01:36:31 -0300

    agregados desafios emprendedor

---
+++ desafios-semana-1/Rentabilidad/emprendedor1.rb
@@ -0,0 +1,13 @@
+#Obtener datos para ejecutar el programa
+price = ARGV[0].to_f
+users_quantity = ARGV[1].to_i
+expenses = ARGV[2].to_f
+
+profit_no_taxes = price * users_quantity - expenses
+
+if profit_no_taxes > 0
+    profit_with_taxes = profit_no_taxes * 0.65
+    puts "Las utilidades son de #{profit_with_taxes}"
+else
+    puts "No obtuviste utilidades, al contrario, perdiste #{profit_no_taxes}"
+end
