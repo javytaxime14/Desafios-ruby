c0d9455359077e01759194d957652eeb47d89ab4
Javiera Hidalgo <javiera_56@hotmail.com>
2020-12-06 18:29:58 -0300

    added desafios arreglo notas, aumento precios y desafio 1

---
+++ desafios-semana-3/desafio_1/desafio_1.rb
@@ -0,0 +1,17 @@
+
+a = [1, 9 ,2, 10, 3, 7, 4, 6]
+
+# Utilizando map sumar 1 a cada uno de los valores del array.
+a.map { |x| x + 1 }
+
+# Utilizando map convertir todos los valores a float.
+a.map { |x| x.to_f }
+
+# Utilizando select descartar todos los elementos menores a 5 en el array.
+a.select { |x| x > 5}
+
+# Utilizando inject sumar todos los valores del array.
+a.inject {|sum, x| x + sum }
+
+# Utilizando .count contar todos los elementos menores que 5.
+a.count {|x| x < 5 }
